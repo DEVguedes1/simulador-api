2dbf135b7f42548b7a795429e8dc8a00a3f22458
Nicolas <nicolasguedesguedes081@gmail.com>
2025-06-17 03:28:56 -0300

    crud finish

--- utilitarios/crud.py
+++ utilitarios/crud.py
@@ -7,3 +7,43 @@ def listar_produtos():
 def listar_usuarios():
     dados = carregar_dados()
     return dados["usuarios"]
+
+def atualizar_produto(id):
+    dados = carregar_dados()
+
+    if id not in dados["produtos"]:
+        print("Produto não encontrado!")
+        return
+    
+    print(f"Editando {dados['id']['nome']}")
+    novo_nome = input(f"Novo nome (atual: {dados['id']['nome']}): ")
+    novo_preco = input(f"Novo preço (atual: {dados['id']['preco']}): ")
+    
+    if novo_nome:
+        dados['id']['nome'] = novo_nome
+    if novo_preco:
+        dados['id']['preco'] = float(novo_preco)
+        
+    salvar_dados(dados)
+    print("Produto atualizado!")
+
+def alualizar_senha(id):
+    dados = carregar_dados()
+
+    if id not in dados["usuarios"]:
+        print("Usuario não encontrado!")
+        return
+    
+    print(f"Editando {dados['id']['nome']}")
+    novo_senha = input(f"Nova senha (atual: {dados['id']['senha']}): ")
+
+    if novo_senha:
+        dados['id']['senha'] =(novo_senha)
+        
+    salvar_dados(dados)
+    print("Produto atualizado!")
+
+def apagar_produto(id):
+    dados= carregar_dados()
+    dados["produtos"]=[p for p in dados["produtos"] if p["id"] != id]
+    salvar_dados(dados)
